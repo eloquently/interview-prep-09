71d4ac0d54ffecbdaf0acfc7c4822819d8b896c0
Eric McKay <eric@andcode.co>
2016-06-10 03:20:12 +0000

    end of guide

--- src/promise_examples.js
+++ src/promise_examples.js
@@ -1,3 +1,34 @@
-// code along with the guide here. 
-// you can run your code with:
-// npm run node src/promise_examples.js
+function slowFunction(cb) {
+    return new Promise((resolve, reject) => {
+        setTimeout(
+            () => resolve(Math.floor(Math.random() * 10)),
+            2000
+        );
+    });
+}
+function slowHare(cb) {
+    return new Promise((resolve, reject) => {
+        setTimeout(
+            () => resolve(Math.floor(Math.random() * 10)),
+            Math.random()*2000 + 1000
+        );
+    });
+}
+
+console.log('start the race!');
+let tortoiseNum, hareNum, sum;
+const tortoisePromise = slowFunction()
+    .then((num) => { 
+        console.log(`I'm a tortoise. My num is ${num}`);
+        tortoiseNum = num;
+    });
+const harePromise = slowHare()
+    .then((num) => { 
+        console.log(`I'm a hare. My num is ${num}`);
+        hareNum = num;
+    });
+
+Promise.all([tortoisePromise, harePromise]).then(() => {
+    sum = hareNum + tortoiseNum;
+    console.log(`Race over! Sum is ${sum}`);
+});
